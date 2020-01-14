e5a867750bad8492e7fbe8cac6dc9b5a2086a10c
pccode21 <16007005@qq.com>
2020-01-14 17:31:51 +0800

    pccode21

--- src/spider/win007_no1.py
+++ src/spider/win007_no1.py
@@ -1,121 +1,90 @@
-import requests
+# 导入需要的包
+from selenium import webdriver
 import time
-import re
-import pymysql
 import traceback
 
-url_time = time.strftime('%Y%m%d%H%M%S')
-# 获取带有时间的url
-url = 'http://zq.win007.com/jsData/matchResult/2019-2020/s36.js?version='+url_time
-# 需要加入跳转网页
-headers = {
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip, deflate',
-            'Host': 'zq.win007.com',
-            'Referer': 'http://zq.win007.com/cn/League/2019-2020/36.html',
-            'Connection': 'keep-alive',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
-            'Cookie': 'bfWin007FirstMatchTime=2020,0,7,08,00,00; win007BfCookie=0^0^1^1^1^1^1^0^0^0^0^0^1^2^1^1^0^1^1^0; UM_distinctid=16f82dd359f17e-0864fb7189f26a-c383f64-100200-16f82dd35a1123; CNZZDATA1261430177=958542710-1578443893-http%253A%252F%252Fzq.win007.com%252F%7C1578465496'
-            }
-# 解析网站
-time.sleep(3)
-l = 0
-try:
-    # get请求可以使用status_code查看访问是否正常
-    response1 = requests.get(url, headers=headers).status_code
-    # 判断网站的正常
-    if response1 == 200:
-        s = requests.session()
-        s.keep_alive = False
-        resp = requests.get(url, headers=headers)
-        # print(resp.text)
-        # 获取球队ID
-        pattern = 'var arrTeam =(.*?);'
-        res = re.search(pattern, resp.text).group(1)
-        res = eval(res)
-        team_id_list = []
-        time.sleep(2)
-        for i in res:
-            team_id = i[0]
-            team_id_list.append(team_id)
-        time.sleep(2)
-        # 取出id 并写入url，跳转球队信息页
-        for i in team_id_list:
-            # 出现异常提示未存入
-            time.sleep(3)
-            headers = {
-                'Referer': 'http://zq.win007.com/cn/team/Summary/{}.html'.format(i),
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'
-            }
-            try:
-                url_qd = 'http://zq.win007.com/jsData/teamInfo/' \
-                          'teamDetail/tdl{}.js?version={}'.format(i, url_time)
-                response1 = requests.get(url_qd, headers=headers).status_code
-                if response1 == 200:
-                    s = requests.session()
-                    s.keep_alive = False
-                    res_qd = requests.get(url_qd, headers=headers)
-                    l = l+1
-                    # 解析数据球队信息,并存入字典
-                    result = {}
-                    pattern = "var teamCharacter =(.*);"
-                    res_age = re.search(pattern, res_qd.text).group(1)
-                    res_age = eval(res_age)
-                    ys = []
-                    fg = []
-                    rd = []
-                    for i in res_age:
-                        if i[0] == 1:
-                            ys.append(i[2])
-                        elif i[0] == 2:
-                            fg.append(i[2])
-                        elif i[0] == 3:
-                            rd.append(i[2])
-                    pattern = 'var teamDetail =(.*)'
-                    res_team = re.findall(pattern, res_qd.text)
-                    pattern = 'var coach =(.*?);'
-                    res_chief = re.search(pattern, res_qd.text).group(1).split(',')
-                    for y in res_team:
-                        res_team = y.split(',')
-                        result['qd_name'] = res_team[1]
-                        result['yy_name'] = res_team[2]
-                        result['el_name'] = res_team[3]
-                        result['thecity'] = res_team[5]
-                        result['home_field'] = res_team[8]
-                        result['set_up'] = res_team[12]
-                        result['coach'] = res_chief[2]
-                        result['address'] = res_team[13]
-                        result['website'] = res_team[-1]
-                        result['ys'] = ys
-                        result['fg'] = fg
-                        result['rd'] = rd
-                        print('已将{}球队信息存入..'.format(result['qd_name']))
-                        # 打开数据库
-                        connect = pymysql.connect('localhost', 'root', 'Lxd05230708', 'football')
-                        # 创建游标
-                        conn = connect.cursor()
-                        # 添加数据
-                        insert_sql = 'insert into team_stats(qd_name, yy_name, el_name, thecity, home_field, set_up, coach, address, website, ys, fg, rd)' \
-                                 ' values ("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")' % (
-                                     result['qd_name'], result['yy_name'],result['el_name'],
-                                     result['thecity'],result['home_field'],result['set_up']
-                                     ,result['coach'] ,result['address'] ,result['website'],
-                                     result['ys'],result['fg'],result['rd'])
-                        # 执行语句
-                        conn.execute(insert_sql)
-                        # 执行事务
-                        connect.commit()
-                        # 关闭数据库
-                        connect.close()
-                        # 关闭游标
-                        conn.close()
-            except:
-                if traceback.format_exc():
-                    print('丢失球员{}的数据'.format(result['qd_name']))
+
+# 发送url请求
+def getDriver(url):
+    try:
+        # 注意chromedriver的版本
+        driver = webdriver.Chrome()
+        driver.get(url)
+        time.sleep(1)
+        return driver
+    except:
+        return ''
+
+
+# 获取子链接列表
+def getURLlist(url, teamURLlist):
+    driver = getDriver(url)
+    try:
+        # 找到包含子链接的所有a标签
+        a_list = driver.find_elements_by_xpath('//table[@id="ScoreGroupTab"]/tbody/tr/td[2]/a')
+        if a_list:
+            for i in a_list:
+                # teamURLlist用于存放子链接
+                teamURLlist.append(i.get_attribute('href'))
+            driver.close()
+            return teamURLlist
         else:
-            print('未进入到球队信息页')
-    else:
-        print('未进入到球队列表页')
-except:
-    if traceback.format_exc():
-        print('丢失数据')
+            return []
+    except:
+        traceback.print_exc()
+        return []
+
+
+# 获取比赛数据
+def getMatchInfo(teamURLlist, fpath):
+    with open(fpath, 'w') as f:
+        # 注意逗号后面不要有空格
+        f.write('比赛,时间,主队,比分,客队,犯规,黄牌,红牌,控球率,射门（射正）,传球（成功）,传球成功率,过人次数,评分\n')
+        if teamURLlist:
+            for url in teamURLlist:
+                driver = getDriver(url)
+                # 表格中数据数据分了好多页，虽然所有数据在driver.page_source中都可见。
+                # 但是，除第一页以外，其他的数据style属性都是“display:none",selenium对这些元素是无法直接操作的。
+                # 我们需要通过JavaScipt 修改display的值
+
+                # 编写一段javascript代码，让浏览器执行。从而把html中的所有tr标签的style属性都设为display='block'
+                js = 'document.querySelectorAll("tr").forEach(function(tr){tr.style.display="block";})'
+                driver.execute_script(js)
+
+                # 接下来，就可以把所有的比赛成绩数据都爬下来
+                infolist = driver.find_elements_by_xpath('//div[@id="Tech_schedule"]/table/tbody/tr')
+
+                # 第一个tr中包含的是表格的title信息，剔除
+                for tr in infolist[1:]:
+                    td_list = tr.find_elements_by_tag_name('td')
+                    matchinfo = []
+                    for td in td_list:
+                        # 部分td的style属性也为“display:none"，info 则对应为‘’，
+                        # 这些td对应的是角球，越位，头球，救球，铲球等信息，不是很重要，就不爬取了。
+                        info = td.text
+                        if info:  # 去除空字符
+                            matchinfo.append(td.text)
+                            matchinfo.append(',')    # 添加逗号作为分隔符
+                    matchinfo.append('\n')  # 在列表最后加上换行符
+
+                    # 将一条比赛信息写入到文件中
+                    f.writelines(matchinfo)
+
+                # 每个网页爬完后，就把打开的浏览器关掉，要不最后会开着好多浏览器窗口。
+                driver.close()
+
+
+def main():
+    # 一级网址：32强分组信息
+    start_url = 'http://zq.win007.com/cn/CupMatch/75.html'
+    # 保存文件及路径
+    output = r'.\PythonLearn\src\spider\worldcup2018.csv'
+    startlist = []
+    resultlist = getURLlist(start_url, startlist)
+    print(resultlist)
+    getMatchInfo(resultlist, output)
+    print('\nfinished\n')
+
+
+if __name__ == '__main__':
+    main()
