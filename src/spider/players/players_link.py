6c07a30008386d13675b67fba9b98a2768af6c26
pccode21 <16007005@qq.com>
2020-01-17 17:21:52 +0800

    pccode21

---
+++ src/spider/players/players_link.py
@@ -0,0 +1,24 @@
+# encoding:UTF-8
+import csv
+import requests
+from bs4 import BeautifulSoup
+with open(r'./PythonLearn/src/spider/players/teamlinks.csv', 'r') as f:  # 取球队链接
+    reader = csv.reader(f)
+    column1 = [row[1]for row in reader]
+player = {}  # 建存储球员链接的字典
+for x in column1:
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'}
+    url = x
+    res = requests.get(url, headers=headers)  # get方法中加入请求头
+    res.encoding = 'gbk'
+    soup = BeautifulSoup(res.text, 'lxml')  # 对返回的结果进行解析
+    player_name = soup.find(class_='sub03_c').find_all('p')  # 找球员姓名
+    for i in range(len(player_name)):
+        player_name[i] = str(player_name[i].get_text())
+    player_links = soup.find(class_='sub03_c').find_all('a')  # 找球员链接
+    for j in range(len(player_links)):
+        player_links[j] = str(player_links[j].attrs['href'])  # 取href属性
+    for k in range(len(player_name)):
+        player[str(player_name[k])] = str(player_links[k])
+with open(r'./PythonLearn/src/spider/players/playerlinks.csv', 'w') as f:  # 写入球员链接
+    [f.write('{0},{1}\n'.format(key, value)) for key, value in player.items()]
---
+++ src/spider/players/teams_link.py
@@ -0,0 +1,32 @@
+# encoding:UTF-8
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_teamlink(url):
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'}
+    res = requests.get(url, headers=headers)  # get方法中加入请求头
+    res.encoding = 'utf-8'  # 设置编码格式防止乱码
+    soup = BeautifulSoup(res.text, 'lxml')  # 对返回的结果进行解析
+    team = soup.select('a[href^="team.php?id"]')  # #匹配 href开头是"team.php?id"的值
+    print(team)
+    print(len(team))
+    team_list = {}
+    for x in range(len(team)):
+        team_list[str(team[x].string)] = str('http://match.sports.sina.com.cn/football/'+team[x].attrs['href'])
+        print(team_list)
+    return (team_list)
+
+
+England = get_teamlink('http://match.sports.sina.com.cn/football/opta_rank.php?year=2019&lid=1')  # 英超联赛的球队链接获取
+Spain = get_teamlink('http://match.sports.sina.com.cn/football/opta_rank.php?year=2019&lid=2')  # 西甲联赛的球队链接获取
+Germany = get_teamlink('http://match.sports.sina.com.cn/football/opta_rank.php?year=2019&lid=3')  # 德甲联赛的球队链接获取
+Italy = get_teamlink('http://match.sports.sina.com.cn/football/opta_rank.php?year=2019&lid=4')  # 意甲联赛的球队链接获取
+French = get_teamlink('http://match.sports.sina.com.cn/football/opta_rank.php?year=2019&lid=5')  # 法甲联赛的球队链接获取
+with open(r'./PythonLearn/src/spider/players/teamlinks.csv', 'w') as f:  # 把数据放到csv文件中
+    [f.write('{0},{1}\n'.format(key, value)) for key, value in England.items()]
+    [f.write('{0},{1}\n'.format(key, value)) for key, value in Spain.items()]
+    [f.write('{0},{1}\n'.format(key, value)) for key, value in Germany.items()]
+    [f.write('{0},{1}\n'.format(key, value)) for key, value in Italy.items()]
+    [f.write('{0},{1}\n'.format(key, value)) for key, value in French.items()]
