26e174776d36e9419853903f096f11e337a10ade
博弈 <16007005@qq.com>
2020-01-18 01:25:33 +0800

    pccode21

--- src/spider/players/players_link.py
+++ src/spider/players/players_link.py
@@ -13,6 +13,7 @@ for x in column1:
     res.encoding = 'gbk'
     soup = BeautifulSoup(res.text, 'lxml')  # 对返回的结果进行解析
     player_name = soup.find(class_='sub03_c').find_all('p')  # 找球员姓名
+    # 因为class是python的关键字，所以在写过滤的时候,凡是写在[ ]里面的都是class，外面的是class_
     for i in range(len(player_name)):
         player_name[i] = str(player_name[i].get_text())
     player_links = soup.find(class_='sub03_c').find_all('a')  # 找球员链接
@@ -20,5 +21,6 @@ for x in column1:
         player_links[j] = str(player_links[j].attrs['href'])  # 取href属性
     for k in range(len(player_name)):
         player[str(player_name[k])] = str(player_links[k])
+        # 将球员姓名和球员链接连接起来，'球员姓名':'球员链接'
 with open(r'./PythonLearn/src/spider/players/playerlinks.csv', 'w') as f:  # 写入球员链接
     [f.write('{0},{1}\n'.format(key, value)) for key, value in player.items()]
