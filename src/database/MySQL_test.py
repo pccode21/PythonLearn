091e6008c61dad3b4a08b121362adfd56c655c5a
博弈 <16007005@qq.com>
2020-01-22 01:24:20 +0800

    pccode21

--- src/database/MySQL_test.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-db=pymysql.connect(host='localhost',user='root',password='Lxd05230708',port=3306,db='test')
-cursor=db.cursor()
-cursor.execute("select * from user")
-row1=cursor.fetchone()
-print(row1)
-db.commit()
-cursor.close()
-db.close()
