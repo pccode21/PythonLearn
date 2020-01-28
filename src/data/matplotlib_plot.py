ee32ed82557feefd2a3866433750de1d13063d6f
博弈 <16007005@qq.com>
2020-01-29 00:03:26 +0800

    pccode21

--- src/data/matplotlib_plot.py
+++ src/data/matplotlib_plot.py
@@ -2,8 +2,10 @@ from matplotlib import pyplot as plt
 import random
 from matplotlib import font_manager
 import matplotlib
+import numpy as np
 
-font = {'family': 'Microsoft Yahei', 'size': '8'}
+
+font = {'family': 'Microsoft Yahei', 'size': '8'}  # 设置才可以正常现在中文
 matplotlib.rc('font', **font)
 # 注释掉的是另一种方法字体设置
 # 路径是需要自己电脑里面的
@@ -15,6 +17,9 @@ matplotlib.rc('font', **font)
 # plt.xticks(x[::2])
 # 当刻度太密集的时候使用列表的步长（间隔取值）来解决，matplotlib会帮助我们对应
 # plt.xticks(range(1, 25))
+# plt.xlim(1, 25)  # 设置x范围
+# x_ticks = np.arange(1, 13, 1)  # 设置x刻度
+# plt.xticks(x_ticks)
 # _xticks_labels = [i/2 for i in range(4, 49)]  # 设置x间隔为0.5
 # plt.xticks(_xticks_labels[::3])  # 取步长每隔3取一个，也就是x间隔为1.5
 # plt.yticks(range(min(y), max(y)+1))
@@ -22,11 +27,16 @@ x = range(120)
 a = [random.randint(20, 35) for i in range(120)]
 y = a
 random.seed(10)
-plt.plot(x, y)
+plt.rcParams['figure.figsize'] = (7.0, 6.5)  # 图片像素
+plt.plot(x, y, linewidth=1, color='green', label='random')  # plot是曲线图，linewidth指定线条粗细
+plt.title('曲线图', fontsize=9)  # 设置标题
+plt.xlabel('时间', fontsize=9)  # 设置x轴标签说明
+plt.ylabel('随机数值', fontsize=9)  # 设置y轴标签说明
+plt.legend()  # 添加图例
 _x_ticks = ['10点{}分'.format(i) for i in x if i<60]
 _x_ticks += ['11点{}分'.format(i-60) for i in x if i>=60]
 plt.xticks(x[::5], _x_ticks[::5], rotation=45)  # rotation设置x轴标签的字体倾斜角度
-plt.Figure(figsize=(20, 20), dpi=80)
+plt.Figure(figsize=(10, 10), dpi=80)
 # 创建自定义图像
 # figsize显示图像显示的位置
 # 在图像模糊的时候，可以传入dpi参数，让图片更加清晰
---
+++ src/data/matplotlib_scatter.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+font = {'family': 'Microsoft Yahei', 'size': '8'}  # 设置才可以正常现在中文
+matplotlib.rc('font', **font)
+# x = [1, 3, 5, 7, 9]
+# y = [2, 4, 6, 8, 10]
+# x = list(range(1, 50))
+# y = [x**2 for x in x]  # y的值是x的平方
+n = 50  # 50个随机数
+x = np.random.normal(0, 1, n)  # 平均值为0，方差为1
+y = np.random.normal(0, 1, n)
+# 散点
+plt.scatter(x, y, linewidth=2, color='blue', label='scatter', alpha=0.5, marker='*')  # scatter是散点图
+plt.legend()  # 添加图例
+# 设置标题
+plt.title("散点图", fontsize=12)
+# 设置x轴
+plt.xlabel("x", fontsize=12)
+# 设置y轴
+plt.ylabel("y", fontsize=12)
+plt.Figure(figsize=(10, 10), dpi=80)
+# 创建自定义图像
+# figsize显示图像显示的位置
+# 在图像模糊的时候，可以传入dpi参数，让图片更加清晰
+thismanager = plt.get_current_fig_manager()
+thismanager.window.wm_iconbitmap(r'./PythonLearn/src/spider/win007/LOGO.ico')
+thismanager.canvas.set_window_title('足球心经的可视化图表')
+# plt.grid(True)  # 设置网格线
+# 显示
+plt.show()
